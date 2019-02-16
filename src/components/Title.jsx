8c94d19855bb6d9426ea6526a5eb2ed4cb18cc93
StartUp <->
2019-02-16 05:26:39 +0400

    added title Component

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,15 +1,5 @@
 import React from 'react'
-import { DEFAULT_ENCODING } from 'crypto';
 
-
-const Title = props => {
-    return (
-        <thead>
-            <tr>
-                <th>{props.title}</th>
-            </tr>
-        </thead>
-    )
-}
+const Title = props => <thead><tr><th>{props.title}</th></tr></thead>
 
 export default Title;
